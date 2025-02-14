c67c6f5761d419316e5b8ecfaaf4fb5ab5fd6b97
Sahil Ambekar <sahil932004ssa@gmail.com>
2025-02-14 13:41:26 +0530

    Username added
    using user of lucide-react

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,13 +1,18 @@
-const SignUpPage = () => {
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { MessageSquare, User, Mail } from "lucide-react";
 
+// import { Link } from "react-router-dom";
+
+const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
-    fullname: '',
-    email: '',
-    password: '',
+    fullname: "",
+    email: "",
+    password: "",
   });
 
-  const {signup, isSigningUp} = useAuthStore();
+  const { signup, isSigningUp } = useAuthStore();
 
   const validateForm = () => {};
   const handleSubmit = (e) => {
@@ -17,7 +22,7 @@ const SignUpPage = () => {
   return (
     <div className="min-h-screen grid lg: grid-cols-2">
       {/* Left side */}
-      <div className="flex flex-col justify-center items-center p-6 sm:p-12"> 
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
           {/* LOGO */}
           <div className="text-center mb-8">
@@ -29,14 +34,37 @@ const SignUpPage = () => {
                 <MessageSquare className="size-6 text-primary" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
-              <p className="text-base-content/60">Get started with your free account</p>
+              <p className="text-base-content/60">
+                Get started with your free account
+              </p>
             </div>
           </div>
         </div>
-
+        {/* FORM */}
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-medium">Full Name</span>
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <User className="size-5 text-base-content/40" />
+              </div>
+              <input
+                type="text"
+                className={`input input-bordered w-full pl-10`}
+                placeholder="John Doe"
+                value={formData.fullName}
+                onChange={(e) =>
+                  setFormData({ ...formData, fullName: e.target.value })
+                }
+              />
+            </div>
+          </div>
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
