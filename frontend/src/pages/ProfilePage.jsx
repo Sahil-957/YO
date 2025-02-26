087ba7acbe30dca8d6b7f8659c5ea6b83c17eb07
Sahil Ambekar <sahil932004ssa@gmail.com>
2025-02-26 23:04:56 +0530

    Revert "profile page changes"
    This reverts commit 3655fdc80f7d55346d6218b528820a1a8b99b4d2.

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
-import { useAuthStore } from "../store/useAuthStore";
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore'
 import { Camera, Mail, User } from "lucide-react";
-import Input from "../components/Input";
-const ProfilePage = () => {
-  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
-  const [selectedImg, setSelectedImg] = useState(null);
-  
 
+const ProfilePage = () => {
+  const { authUser, isUpdatingProfile , UpdateProfile } = useAuthStore()
+  const [selectedImg, setSelectedImg] = useState(null);
   const handleImageUpload = async (e) => {
     const file = e.target.files[0];
-    if (!file) return;  
+    if (!file) return;
 
     const reader = new FileReader();
 
@@ -20,15 +18,15 @@ const ProfilePage = () => {
       setSelectedImg(base64Image);
       await updateProfile({ profilePic: base64Image });
     };
-  };
+  }
 
   return (
-    <div className="h-screen pt-20">
-      <div className="max-w-2xl mx-auto p-4 py-8 ">
-        <div className="bg-base-300 rounded-xl p-6 space-y-8">
-          <div className="text-center">
-            <h1 className="text-2xl font-semisolid">profile</h1>
-            <p className="mt-2">Your profile information</p>
+    <div className='h-screen pt-20'>
+      <div className='max-w-2xl mx-auto p-4 py-8 '>
+        <div className='bg-base-300 rounded-xl p-6 space-y-8'>
+          <div className='text-center'>
+            <h1 className='text-2xl font-semisolid'>profile</h1>
+            <p className='mt-2'>Your profile information</p>
           </div>
 
           {/* Profile Image */}
@@ -46,9 +44,7 @@ const ProfilePage = () => {
                   bg-base-content hover:scale-105
                   p-2 rounded-full cursor-pointer 
                   transition-all duration-200
-                  ${
-                    isUpdatingProfile ? "animate-pulse pointer-events-none" : ""
-                  }
+                  ${isUpdatingProfile ? "animate-pulse pointer-events-none" : ""}
                 `}
               >
                 <Camera className="w-5 h-5 text-base-200" />
@@ -63,38 +59,17 @@ const ProfilePage = () => {
               </label>
             </div>
             <p className="text-sm text-zinc-400">
-              {isUpdatingProfile
-                ? "Uploading..."
-                : "Click the camera icon to update your photo"}
+              {isUpdatingProfile ? "Uploading..." : "Click the camera icon to update your photo"}
             </p>
           </div>
 
           <div className="space-y-6">
-            {/* <Input icon={User}>
-              {{
-                description: "Username:",
-                placeholder: "Enter your username",
-                id: "username",
-                type: "text",
-              }}
-            </Input>
-            <Input icon={Mail}>
-              {{
-                description: "Mail",
-                placeholder: "Enter your Mail",
-                id: "mail",
-                type: "text",
-              }}
-            </Input> */}
             <div className="space-y-1.5">
               <div className="text-sm text-zinc-400 flex items-center gap-2">
                 <User className="w-4 h-4" />
                 Full Name
-              </div> 
-
-              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
-                {authUser?.fullName}
-              </p>
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.fullName}</p>
             </div>
 
             <div className="space-y-1.5">
@@ -102,9 +77,7 @@ const ProfilePage = () => {
                 <Mail className="w-4 h-4" />
                 Email Address
               </div>
-              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
-                {authUser?.email}
-              </p>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.email}</p>
             </div>
           </div>
 
@@ -121,10 +94,11 @@ const ProfilePage = () => {
               </div>
             </div>
           </div>
+
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProfilePage;
+export default ProfilePage
